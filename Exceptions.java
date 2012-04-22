9bb9ea45a28a4f139befcd47dd9dcdcc0ec0f29e
Yuechen Zhao <yuechenzhao1993@gmail.com>
2012-04-22 15:53:33 -0400

    Added Exceptions.java

---
+++ Exceptions.java
@@ -0,0 +1,14 @@
+/**
+ * An exception for when a part of speech is not found, such as when the user gives an  
+ * incomplete list of parts of speech.
+ */
+class POSNotFoundException extends Exception
+{
+}
+
+/**
+ * An exception for when a file given is in the incorrect format.
+ */
+class WrongFormatException extends Exception
+{
+}
